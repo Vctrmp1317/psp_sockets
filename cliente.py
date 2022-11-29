c164bc5fbc9c4c7cc30e030c6ff83d06490a9d1b
Chette <80092129+MrChette@users.noreply.github.com>
2022-11-29 22:15:50 +0100

    probando cosas

--- cliente.py
+++ cliente.py
@@ -2,20 +2,25 @@
 import socket
 
 s = socket.socket()
-s.connect(("localhost", 9003))
 
-correo = input('Introduce el correo electronico')
-s.send(correo.encode())
-password = input('Introduce la contraseña')
-s.send(password.encode())
-if(s.recv(1024).decode()==True):
-    nick= input('Escribe tu nick de partida')
-    s.send(nick.encode())
+
+# correo = input('Introduce el correo electronico')
+# s.send(correo.encode())
+# password = input('Introduce la contraseña')
+# s.send(password.encode())
+
+# if(s.recv(1024).decode()==True):
+#     nick= input('Escribe tu nick de partida')
+#     s.send(nick.encode())
+
+nick= input('Escribe tu nick de partida')
+s.connect(("localhost", 9003))
+s.send(nick.encode())
 
 fin_partida=True
 while fin_partida:
-    print ('Partida empezada')
-    fin_partida=False
+    a = 1
+    # fin_partida=False
 
 
 s.close()
